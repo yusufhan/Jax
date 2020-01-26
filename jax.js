0522d6e5b484b18dc40b3bf6232655f1b908483a
Yusufhan Karakoç <46138164+yusufhan@users.noreply.github.com>
2020-01-26 17:55:43 +0300

    Update jax.js

--- jax.js
+++ jax.js
@@ -1,23 +1,45 @@
-﻿function main() {
-    var a, toplam;
+function main() {
+    var sayi, i, toplam, j, asalmi, dizi, eleman;
     
-    toplam = 0;
-    a = window.prompt('Enter a value for a');
-    if (a % 2 == 0) {
-        toplam = toplam + 2;
+    dizi = 0;
+    asalmi = 0;
+    toplam = 1;
+    sayi = window.prompt('Enter a value for sayi');
+    for (i = 2; i <= sayi - 1; i++) {
+        for (j = 2; j <= sayi - 1; j++) {
+            if (i % j == 0) {
+                asalmi = asalmi + 1;
+            }
+        }
+        if (asalmi <= 1) {
+            dizi = dizi + 1;
+        }
+        asalmi = 0;
     }
-    if (a % 3 == 0) {
-        toplam = toplam + 3;
+    var asallar = createArray(dizi);
+    
+    asalmi = 0;
+    eleman = 0;
+    for (i = 2; i <= sayi - 1; i++) {
+        for (j = 2; j <= sayi - 1; j++) {
+            if (i % j == 0) {
+                asalmi = asalmi + 1;
+            }
+        }
+        if (asalmi <= 1) {
+            asallar[eleman] = i;
+            eleman = eleman + 1;
+        }
+        asalmi = 0;
     }
-    if (a % 5 == 0) {
-        toplam = toplam + 5;
+    for (i = 0; i <= dizi - 1; i++) {
+        if (sayi % i == 0) {
+            toplam = toplam + asallar[i];
+        }
     }
-    if (a % 7 == 0) {
-        toplam = toplam + 7;
-    }
-    if (toplam == a) {
-        window.alert(a + " is a Jax Number.");
+    if (toplam == sayi) {
+        window.alert("Girilen " + sayi + " değeri Jax sayısıdır.");
     } else {
-        window.alert(a + "isn't a Jax Number.");
+        window.alert("Girilen " + sayi + " değeri Jax sayısı değildir.");
     }
 }
